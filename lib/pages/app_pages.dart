2888ced60ce40f1aefd88e20c44c90240efe3911
allansrc <allanramos.teixeira@gmail.com>
2021-01-26 20:02:18 -0300

    update

--- lib/pages/app_pages.dart
+++ lib/pages/app_pages.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 import 'package:rotappcismo/modules/home/home_page.dart';
 import 'package:rotappcismo/modules/memo_g/memo_game_page.dart';
 import 'package:rotappcismo/modules/word_complete/word_complete_page.dart';
+import 'package:rotappcismo/modules/word_matches/word_match.dart';
 import 'package:rotappcismo/routes/app_routes.dart';
 
 class AppPages {
@@ -9,5 +10,6 @@ class AppPages {
     GetPage(name: AppRoutes.initial, page: () => HomePage()),
     GetPage(name: AppRoutes.memog, page: () => MemogamePage()),
     GetPage(name: AppRoutes.wordcomp, page: () => WordCompletePage()),
+    GetPage(name: AppRoutes.wordmatch, page: () => WordMatchPage()),
   ];
 }
--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -2,4 +2,5 @@ class AppRoutes {
   static const initial = '/';
   static const memog = '/memo_g';
   static const wordcomp = '/wordcomp';
+  static const wordmatch = '/wordmatch';
 }
